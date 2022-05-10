a4462fbe5e1d2f765d09657b0c619a69b8af3080
Guillote1233 <98848165+Guillote1233@users.noreply.github.com>
2022-05-10 14:35:21 -0300

    agrego header

--- ProyectoIntegrador_AlvarezGuillermo/src/app/app.module.ts
+++ ProyectoIntegrador_AlvarezGuillermo/src/app/app.module.ts
@@ -3,10 +3,18 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './components/header/header.component';
+import { HomeLogoComponent } from './components/home-logo/home-logo.component';
+import { SocialMediaComponent } from './components/social-media/social-media.component';
+import { BannerComponent } from './components/banner/banner.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    HomeLogoComponent,
+    SocialMediaComponent,
+    BannerComponent
   ],
   imports: [
     BrowserModule,
