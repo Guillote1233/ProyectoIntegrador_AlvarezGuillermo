5679fd8c2b9f3c58694da134a7677ed882d93fa4
Guillote1233 <98848165+Guillote1233@users.noreply.github.com>
2022-05-29 19:21:05 -0300

    backend

---
+++ ProyectoIntegrador_AlvarezGuillermo/Backend/src/test/java/com/portfolio/guillermoalvarez/GuillermoalvarezApplicationTests.java
@@ -0,0 +1,13 @@
+package com.portfolio.guillermoalvarez;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GuillermoalvarezApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
